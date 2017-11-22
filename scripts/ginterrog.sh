cad9679326c0dace50d54adf6423f99231f8f453
artem <komisyk@gmail.com>
2017-11-23 01:58:49 +0300

    new edit 23.11.2017

---
+++ scripts/ginterrog.sh
@@ -0,0 +1,12 @@
+#!/bin/dash
+
+IECDIR="/opt/iecd_with_proxy"
+IECDB="$IECDIR/iecdb"
+
+if ! [ -z "$(ls $IECDB)" ]; then 
+	find $IECDB -type f -printf "%P\n" | \
+	while IFS="/" read comaddr type objaddr; do echo $comaddr $objaddr $type $(cat $comaddr/$type/$objaddr 2>/dev/null); done | \
+	$IECDIR/asduconv.sh -c 20
+else
+	exit 1
+fi
