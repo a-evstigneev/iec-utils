c4b0c5ea634d9715961c7226988ca268ef12ef08
artem <komisyk@gmail.com>
2017-11-23 02:30:43 +0300

    new edit 23.11.2017

--- scripts/ginterrog.sh
+++ scripts/ginterrog.sh
@@ -5,8 +5,13 @@ IECDB="$IECDIR/iecdb"
 
 if ! [ -z "$(ls $IECDB)" ]; then 
 	find $IECDB -type f -printf "%P\n" | \
-	while IFS="/" read comaddr type objaddr; do echo $comaddr $objaddr $type $(cat $comaddr/$type/$objaddr 2>/dev/null); done | \
-	$IECDIR/asduconv.sh -c 20
+	while IFS="/" read comaddr type objaddr;do 
+		echo $comaddr $objaddr $type $(cat $IECDB/$comaddr/$type/$objaddr 2>/dev/null) 
+	done | $IECDIR/asduconv.sh -c 20
 else
 	exit 1
 fi
+
+
+#find $IECDB -type f -printf "%P\n" | while IFS="/" read comaddr type objaddr; do echo $comaddr $objaddr $type $(cat $IECDB/$comaddr/$type/$objaddr 2>/dev/null); done # | \
+#$IECDIR/asduconv.sh -c 20
