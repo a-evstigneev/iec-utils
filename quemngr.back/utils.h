7d5c5665ea582c640a9a288f3ff6a658362d6c3f
Evstigneev Artem <komisyk@gmail.com>
2018-04-03 00:59:39 +0300

    New_edit_2.04.2018

--- quemngr.back/utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-int mvfile(const char *src, const char *dst);
-int fsyncdir(const char *dir);
-int calctimeout(const char *dir);
-struct timeval *getmonotime(struct timeval *tv);
-
-#endif
