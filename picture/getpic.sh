1f8f58106e91f8bc4d068b9d8ca2ebda1d652da4
Artem Evstigneev <komisyk@gmail.com>
2018-07-03 01:57:38 +0300

    03.07.2018

--- picture/getpic.sh
+++ picture/getpic.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-iconv -futf8 -tkoi8r pic6 | preconv -ekoi8r | groff -p -Tps -mru > pic6.ps
+iconv -futf8 -tkoi8r smsget   | preconv -ekoi8r | groff -p -Tps -mru > smsget.ps
+iconv -futf8 -tkoi8r quemngr  | preconv -ekoi8r | groff -p -Tps -mru > quemngr.ps
+iconv -futf8 -tkoi8r iecproxy | preconv -ekoi8r | groff -p -Tps -mru > iecproxy.ps
+ps2pdf smsget.ps
+ps2pdf quemngr.ps
+ps2pdf iecproxy.ps
+rm *.ps
